093ba17ff60f9d5b98ec5c0e3309d84777f6694b
Данил Шипилов <danil.shipilov.99@gmail.com>
2019-07-01 00:12:16 +0600

    NP_MulticastChat

---
+++ MyMulticastChat/Chat.cs
@@ -0,0 +1,207 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MyMulticastChat
+{
+    public class Chat
+    {
+        MainWindow mainWindow;
+
+        private Guid myId;
+        private string myName;
+
+        private List<User> allUsers;
+
+        private bool alive;
+        private UdpClient udpClient;
+        private IPAddress multicastAddress;
+        private IPEndPoint remoteep;
+
+        public Chat(MainWindow mainWindow, string myName)
+        {
+            this.mainWindow = mainWindow;
+            myId = Guid.NewGuid();
+            this.myName = myName;
+            allUsers = new List<User>();
+            alive = false;
+            multicastAddress = IPAddress.Parse("239.0.0.0");
+            udpClient = new UdpClient();
+            udpClient.JoinMulticastGroup(multicastAddress);
+            remoteep = new IPEndPoint(multicastAddress, 12345);
+        }
+
+        public void SendMessage(string data)
+        {
+            Byte[] buffer = Encoding.UTF8.GetBytes(data);
+            udpClient.Send(buffer, buffer.Length, remoteep);
+        }
+
+        public void Listen()
+        {
+            alive = true;
+            udpClient = new UdpClient();
+            udpClient.ExclusiveAddressUse = false;
+            IPEndPoint localEp = new IPEndPoint(IPAddress.Any, 12345);
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.Client.Bind(localEp);
+            udpClient.JoinMulticastGroup(multicastAddress);
+
+            try
+            {
+                while (alive)
+                {
+                    Byte[] data = udpClient.Receive(ref localEp);
+                    DefineAttributeBehavior(Encoding.UTF8.GetString(data));
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        public void DefineAttributeBehavior(string attributeString)
+        {
+            string[] substrings = attributeString.Split(new char[] { (char)1 }, StringSplitOptions.RemoveEmptyEntries);
+
+            switch (substrings[0])
+            {
+                case "Message":
+                    MessageCome(substrings[1], substrings[2]);
+                    break;
+                case "Signin":
+                    SigninUser(substrings[1]);
+                    break;
+                case "Signout":
+                    SignoutUser(substrings[1]);
+                    break;
+                case "ListUsers":
+                    AllUsgersList(substrings[1]);
+                    break;
+            }
+        }
+
+        public void MessageCome(string userName, string message)
+        {
+            mainWindow.Dispatcher.Invoke(new ThreadStart(() => mainWindow.allMessageTextBox.Text += DateTime.Now.ToShortTimeString() + ": " + userName + " - " + message + "\r\n"));
+        }
+
+        public void AllUsgersList(string jsonAllUser)
+        {
+            List<User> allUsersList = JsonConvert.DeserializeObject<List<User>>(jsonAllUser);
+            if (allUsersList.LastOrDefault().UserId == myId)
+            {
+                allUsers = allUsersList;
+                UpdateOnlineUsersListBox();
+            }
+        }
+
+        public void SignoutUser(string jsonUser)
+        {
+            User signoutUser = JsonConvert.DeserializeObject<User>(jsonUser);
+            if (myId != signoutUser.UserId)
+            {
+                for (int i = 0; i < allUsers.Count; i++)
+                {
+                    if (allUsers[i].UserId == signoutUser.UserId)
+                    {
+                        allUsers.Remove(allUsers[i]);
+                        break;
+                    }
+                }
+                mainWindow.Dispatcher.Invoke(new ThreadStart(() => mainWindow.allMessageTextBox.Text += DateTime.Now.ToShortTimeString() + ": " + signoutUser.UserName + " вышел из чата" + "\r\n"));
+                UpdateOnlineUsersListBox();
+            }
+        }
+
+        public void SigninUser(string jsonUser)
+        {
+            User signinUser = JsonConvert.DeserializeObject<User>(jsonUser);
+            allUsers.Add(signinUser);
+            mainWindow.Dispatcher.Invoke(new ThreadStart(() => mainWindow.allMessageTextBox.Text += DateTime.Now.ToShortTimeString() + ": " + signinUser.UserName + " вошел в чат" + "\r\n"));
+            UpdateOnlineUsersListBox();
+            if (allUsers.FirstOrDefault().UserId == myId && allUsers.LastOrDefault().UserId != myId)
+            {
+                SendAllUsersToTheConnectedUser();
+            }
+        }
+
+        public void SendAllUsersToTheConnectedUser()
+        {
+            string jsonString = JsonConvert.SerializeObject(allUsers);
+            SendMessage(FormAttributeString(Attribute.ListUsers, jsonString));
+        }
+
+        public void ThisUserConnection()
+        {
+            string jsonString = JsonConvert.SerializeObject(new User { UserId = myId, UserName = myName });
+            SendMessage(FormAttributeString(Attribute.Signin, jsonString));
+        }
+
+        public void SendMessageToTheAllUsers(string message)
+        {
+            SendMessage(FormAttributeString(Attribute.Message, myName, message));
+            mainWindow.messageTextBox.Text = string.Empty;
+        }
+
+        public string FormAttributeString(Attribute attribute, params string[] messageBlocks)
+        {
+            string attributeString = attribute.ToString();
+            for (int i = 0; i < messageBlocks.Length; i++)
+            {
+                attributeString += (char)1 + messageBlocks[i];
+            }
+            return attributeString;
+        }
+
+        public void ThisUserDisconnection()
+        {
+            if (alive)
+            {
+                alive = false;
+
+                string jsonString = JsonConvert.SerializeObject(new User { UserId = myId, UserName = myName });
+                SendMessage(FormAttributeString(Attribute.Signout, jsonString));
+                Thread.Sleep(5);
+                udpClient.DropMulticastGroup(multicastAddress);
+
+                udpClient.Close();
+            }
+        }
+
+        public void UpdateOnlineUsersListBox()
+        {
+            int maxLength = GetMaxLenghtUserName();
+
+            mainWindow.Dispatcher.Invoke(new ThreadStart(() => mainWindow.onlineUsersListBox.Items.Clear()));
+
+            foreach (var user in allUsers)
+            {
+                mainWindow.Dispatcher.Invoke(new ThreadStart(() => mainWindow.onlineUsersListBox.Items.Add(String.Format("{0,-" + (maxLength + 5) + "}", user.UserName) + user.UserId.ToString())));
+            }
+        }
+
+        private int GetMaxLenghtUserName()
+        {
+            int maxLength = 0;
+            foreach (var user in allUsers)
+            {
+                if (user.UserName.Length > maxLength)
+                {
+                    maxLength = user.UserName.Length;
+                }
+            }
+            return maxLength;
+        }
+    }
+}
---
+++ MyMulticastChat/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyMulticastChat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Chat chat;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^ЁёА-яa-zA-Z0-9]");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void SigninButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (userNameTextBox.Text.Trim() == string.Empty)
+                return;
+
+            var userName = userNameTextBox.Text.Trim();
+            userName = userName.Replace(" ", "_");
+            userNameTextBox.Text = userName;
+            ElementsMode(true);
+
+            chat = new Chat(this, userName);
+            Thread listenThread = new Thread(new ThreadStart(chat.Listen));
+            listenThread.IsBackground = true;
+            listenThread.Start();
+            Thread.Sleep(5);
+            chat.ThisUserConnection();
+        }
+
+        private void ElementsMode(bool isConnected)
+        {
+            if (isConnected)
+                label1.Content = "Ваше имя";
+            else
+            {
+                label1.Content = "Введите имя";
+                userNameTextBox.Text = string.Empty;
+                onlineUsersListBox.Items.Clear();
+            }
+
+            allMessageTextBox.Text = string.Empty;
+            messageTextBox.Text = string.Empty;
+
+            userNameTextBox.IsReadOnly = isConnected;
+            signinButton.IsEnabled = !isConnected;
+            signoutButton.IsEnabled = isConnected;
+            allMessageTextBox.IsEnabled = isConnected;
+            messageTextBox.IsEnabled = isConnected;
+            sendButton.IsEnabled = isConnected;
+
+        }
+
+        private void SignoutButtonClick(object sender, RoutedEventArgs e)
+        {
+            ElementsMode(false);
+            chat.ThisUserDisconnection();
+        }
+
+        private void SendButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (messageTextBox.Text.Trim() != string.Empty)
+            {
+                chat.SendMessageToTheAllUsers(messageTextBox.Text);
+            }
+        }
+
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(chat != null)
+            chat.ThisUserDisconnection();
+        }
+    }
+}
